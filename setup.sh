cf4982ac10fdb6417e8f9a1fbb11bbd6d636ad7f
Skylar Tamke <skylar4494@gmail.com>
2017-02-24 10:56:45 -0700

    updated setup to actually write to rc.local

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-printf "\n/home/pi/Desktop/raspkill/timer.sh" >> /etc/rc.local
+sudo sed -i -e '$i /home/pi/Desktop/raspkill/timer.sh \n' /etc/rc.local
 cd
 cd Desktop
 git clone https://github.com/MSU-BOREALIS/Raspikill.git
-sleep 10s
+
 printf "cloning complete\n"
 
 
